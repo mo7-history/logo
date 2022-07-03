f0114a13236c9ca02259c63a7abecc731405b7dc
墨七 <meichangliang@mo7.cc>
2022-07-04 01:53:03 +0800

    exit-push

--- _shell/init.sh
+++ _shell/init.sh
@@ -8,3 +8,12 @@ path=$(pwd)
 # 项目的名字和编译时的名字
 startName=${path##*/}
 buildName=${startName}
+
+# 最终的输出目录
+outPutPath=${path}"/dist"
+
+# 前端文件输出目录
+staticPath=${path}"/www"
+
+# 部署目录
+deployPath="/root/ProdProject/VueDemo.net"
